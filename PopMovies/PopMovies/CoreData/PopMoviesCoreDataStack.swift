1ec5b5eb557baaaf3ea14ee084f259f89dd0acdb
Angela Alves <angelavbalves@rede.ulbra.br>
2022-10-18 10:14:57 -0300

    feat: coredata stack

---
+++ PopMovies/PopMovies/CoreData/PopMoviesCoreDataStack.swift
@@ -0,0 +1,64 @@
+//
+//  PopMoviesCoreDataStack.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 13/10/22.
+//
+
+import Foundation
+import CoreData
+
+open class PopMoviesCoreDataStack: PopMoviesCoreDataProtocol {
+
+    var modelName = "FavoriteMovies"
+    var entityName = "FavoriteMovieItem"
+
+    lazy var model: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd") else {
+            return NSManagedObjectModel()
+        }
+        return NSManagedObjectModel(contentsOf: modelURL) ?? NSManagedObjectModel()
+    }()
+
+    lazy var mainContext: NSManagedObjectContext = {
+        self.storeContainer.viewContext
+    }()
+
+    lazy var storeContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: entityName, managedObjectModel: model)
+
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+
+        return container
+    }()
+
+    public func saveContext(_ context: NSManagedObjectContext) {
+        if context != mainContext {
+            saveDerivedContext(context)
+            return
+        }
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+
+    public func saveDerivedContext(_ context: NSManagedObjectContext) {
+        context.perform {
+            do {
+                try context.save()
+            } catch let error as NSError {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        self.saveContext(self.mainContext)
+    }
+
+}
