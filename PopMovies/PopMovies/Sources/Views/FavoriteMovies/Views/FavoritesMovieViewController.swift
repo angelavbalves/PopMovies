e85523b4f9a834874cda215c81f610278030b67c
Angela Alves <angelavbalves@rede.ulbra.br>
2022-10-21 18:35:28 -0300

    feat: favorite movies view controller

---
+++ PopMovies/PopMovies/Sources/Views/FavoriteMovies/Views/FavoritesMovieViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesMoviesViewController.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 20/10/22.
+//
+
+import Foundation
+import UIKit
+
+class FavoritesMoviesViewController: PMViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
