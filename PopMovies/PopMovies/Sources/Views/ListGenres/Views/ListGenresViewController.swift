751db642a7ba36fa24708681e538b6ee8c806a82
Angela Alves <angelavbalves@rede.ulbra.br>
2022-10-21 18:35:43 -0300

    feat: list genres view controller

---
+++ PopMovies/PopMovies/Sources/Views/ListGenres/Views/ListGenresViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListGenresViewController.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 20/10/22.
+//
+
+import Foundation
+import UIKit
+
+class ListGenresViewController: PMViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
