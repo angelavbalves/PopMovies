980e317072db478257d3f9259fe6b29a8c02443e
Angela Alves <angelavbalves@rede.ulbra.br>
2022-10-25 16:33:00 -0300

    feat: coordinator

---
+++ PopMovies/PopMovies/Sources/Common/Coordinators/CoordinatorProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  CoordinatorProtocol.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 24/10/22.
+//
+
+import Foundation
+import UIKit
+
+protocol CoordinatorProtocol {
+    var rootViewController: UIViewController? { get }
+    func start()
+}
