78b8f433c45b07847c70ab2a5c71919aec5a9103
Angela Alves <angelavbalves@rede.ulbra.br>
2022-10-25 16:33:48 -0300

    feat: movie details ViewModel

---
+++ PopMovies/PopMovies/Sources/Views/MovieDetails/ViewModel/MovieDetailsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MovieDetailsViewModel.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 24/10/22.
+//
+
+import Foundation
+import UIKit
+
+class MovieDetailsViewModel {
+
+    weak var coordinator: AppCordinator?
+
+    init(coordinator: AppCordinator) {
+        self.coordinator = coordinator
+    }
+}
