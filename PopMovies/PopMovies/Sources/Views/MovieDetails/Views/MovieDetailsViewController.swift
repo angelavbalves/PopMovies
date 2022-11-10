6e2f8ee1e138d5270a6956af7fc2000f1b88096f
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-10 17:29:40 -0300

    feat: details view controller and view

--- PopMovies/PopMovies/Sources/Views/MovieDetails/Views/MovieDetailsViewController.swift
+++ PopMovies/PopMovies/Sources/Views/MovieDetails/Views/MovieDetailsViewController.swift
@@ -11,15 +11,25 @@ import UIKit
 class MovieDetailsViewController: PMViewController {
 
     var movie: MovieItem
+    var isFavorite: Bool
     let viewModel: MovieDetailsViewModel
-    private lazy var detailsView = MovieDetailsView(movie: movie, fetchSimilarMovies: getSimilarMovies)
+    private lazy var detailsView = MovieDetailsView(
+        movie: movie,
+        isFavorite: isFavorite,
+        fetchSimilarMovies: getSimilarMovies,
+        favoriteButtonSelectedAction: buttonSelected(_:),
+        favoriteButtonUnselectedAction: buttonUnselected(_:)
+    )
 
     init(movie: MovieItem,
-        viewModel: MovieDetailsViewModel
-    ) {
+         isFavorite: Bool,
+         viewModel: MovieDetailsViewModel)
+    {
         self.movie = movie
+        self.isFavorite = isFavorite
         self.viewModel = viewModel
         super.init()
+        self.movie.isFavorite = self.isFavorite
     }
 
     override func loadView() {
@@ -27,7 +37,10 @@ class MovieDetailsViewController: PMViewController {
     }
 
     override func viewDidLoad() {
+        super.viewDidLoad()
         getSimilarMovies()
+        title = "\(movie.title)"
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
 
     func getSimilarMovies() {
@@ -38,8 +51,18 @@ class MovieDetailsViewController: PMViewController {
                 case .error:
                     print("Error to get similar movies")
             }
-
         }
     }
 
+    func didTapOnMovieAction(_ movie: MovieItem, _ isFavorite: Bool) {
+        viewModel.coordinator?.routeToDetails(of: movie, is: isFavorite)
+    }
+
+    func buttonSelected(_ movie: MovieItem) {
+        viewModel.saveMovieInCoreData(movie)
+    }
+
+    func buttonUnselected(_ id: Int) {
+        viewModel.removeMovieOfCoreData(for: id)
+    }
 }
