829b9b86c7bf93e0f99e1286f20f07d4cadca6f0
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-18 18:45:34 -0300

    feat: builds route coordinator for favorite movies view

---
+++ PopMovies/PopMovies/Sources/Views/FavoriteMovies/FavoriteMoviesCoordinator.swift
@@ -0,0 +1,38 @@
+//
+//  FavoriteMoviesCoordinator.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 11/11/22.
+//
+import Foundation
+import UIKit
+
+class FavoriteMoviesCoordinator: CoordinatorProtocol {
+
+    // MARK: - Properties
+    var rootViewController: UINavigationController?
+    private var childCoordinator: [CoordinatorProtocol] = []
+
+    // MARK: - Init
+    init(
+        rootViewController: PMNavigationController = .init()
+    ) {
+        self.rootViewController = rootViewController
+    }
+
+    // MARK: - Start method
+    func start() {
+        let favoriteMoviesViewModel = FavoriteMoviesViewModel(coordinator: self)
+        let favoriteMoviesViewController = FavoritesMoviesViewController(viewModel: favoriteMoviesViewModel)
+
+        rootViewController?.setViewControllers([favoriteMoviesViewController], animated: false)
+    }
+
+    // MARK: - Route
+    func routeToDetails(of movie: MovieItem) {
+        let coordinator = MovieDetailsCoordinator(movie: movie, parentNavigation: rootViewController)
+
+        childCoordinator.append(coordinator)
+        coordinator.start()
+    }
+}
