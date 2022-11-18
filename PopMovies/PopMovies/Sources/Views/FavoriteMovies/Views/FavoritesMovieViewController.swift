6bb098a265e3ea3b502bc33ba0710e0925744c04
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-18 18:50:39 -0300

    feat: routeToDetails in the favorite controller

--- PopMovies/PopMovies/Sources/Views/FavoriteMovies/Views/FavoritesMovieViewController.swift
+++ PopMovies/PopMovies/Sources/Views/FavoriteMovies/Views/FavoritesMovieViewController.swift
@@ -10,33 +10,47 @@ import UIKit
 
 class FavoritesMoviesViewController: PMViewController {
 
+    // MARK: - ViewModel
     let viewModel: FavoriteMoviesViewModel
 
-    private lazy var tableView = FavoriteMoviesView(removeFavoriteMovie: removeFavoriteMovie(for:))
+    // MARK: - View
+    private lazy var tableView = FavoriteMoviesView(
+        removeFavoriteMovie: removeFavoriteMovie(for:),
+        didTapOnMovie: didTapOnFavoriteMovieAction(_:)
+    )
 
-    override func loadView() {
-        view = tableView
-    }
-
+    // MARK: - Init
     init(viewModel: FavoriteMoviesViewModel) {
         self.viewModel = viewModel
         super.init()
     }
 
+    // MARK: - Life Cycle
+    override func loadView() {
+        view = tableView
+    }
+
     override func viewDidLoad() {
+        super.viewDidLoad()
         title = "Favorite Movies"
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         let favoriteMovies = getFavoriteMovies()
         tableView.popularFavoriteMoviesList(with: favoriteMovies)
     }
 
+    // MARK: - Aux
     func getFavoriteMovies() -> [MovieItem] {
         viewModel.fetchFavoritesMovies()
     }
 
     func removeFavoriteMovie(for id: Int) {
         viewModel.removeFavoriteMovie(for: id)
+    }
+
+    func didTapOnFavoriteMovieAction(_ movie: MovieItem) {
+        viewModel.routeToDetails(of: movie)
     }
 }
