baff6b429299a5db412d8fc5021059889c0929b3
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-22 15:10:17 -0300

    feat: builds the MoviesByGenreViewModel

---
+++ PopMovies/PopMovies/Sources/Views/MoviesByGenre/ViewModel/MoviesByGenreViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  MoviesByGenreViewModel.swift
+//  PopMovies
+//
+//  Created by Angela Alves on 22/11/22.
+//
+
+import Foundation
+import UIKit
+
+class MoviesByGenreViewModel {
+    // MARK: - Properties
+    private let service: PMService
+    private let favoriteService: FavoriteMoviesService
+    private var currentPage = 1
+    private weak var coordinator: MoviesByGenreCoordinator?
+    let name: String
+    private let id: Int
+
+    // MARK: - Init
+    init(
+        id: Int,
+        name: String,
+        service: PMService = .live(),
+        favoriteService: FavoriteMoviesService = .live(),
+        coordinator: MoviesByGenreCoordinator
+    ){
+        self.id = id
+        self.name = name
+        self.coordinator = coordinator
+        self.service = service
+        self.favoriteService = favoriteService
+    }
+
+    // MARK: - Aux
+    func getMovies(_ completion: @escaping (PopMoviesState) -> Void) {
+        service.listMoviesByGenre(currentPage, id) { result in
+            DispatchQueue.main.async {
+                switch result {
+                    case .success(let response):
+                        let movies = response.results.map(MovieItem.init)
+                        completion(.success(movies))
+                    case .failure:
+                        completion(.error)
+                }
+            }
+        }
+    }
+
+    func saveMovieInCoreData(_ movie: MovieItem) {
+        favoriteService.saveMovie(movie)
+    }
+
+    func removeMovieOfCoreData(for id: Int) {
+        favoriteService.removeMovie(id)
+    }
+
+    func verifyMovieInCoreData(for id: Int) -> Bool {
+        favoriteService.verifyIfMovieIsInCoreData(id)
+    }
+
+    func routeToDetails(of movie: MovieItem) {
+        coordinator?.routeToDetails(of: movie)
+    }
+}
