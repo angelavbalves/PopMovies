b6b2c129998ac1e7fb6ac1b4b61ce207541925ab
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-23 12:01:26 -0300

    feat: include tests to list genres, get movies by genre and test decode

--- PopMovies/PopMoviesTests/Service/PMServiceTests.swift
+++ PopMovies/PopMoviesTests/Service/PMServiceTests.swift
@@ -39,6 +39,23 @@ class PMServiceTests: XCTestCase {
         }
     }
 
+    func test_whenGetGenres_shouldSucessfullDecode() {
+        guard let mockFile = Bundle(for: type(of: self)).url(forResource: "GenreResponse+Fixture", withExtension: "json") else {
+            XCTFail("Mock file not found")
+            return
+        }
+
+        apiClientMock.mockFile = mockFile
+        sut = .live(apiClientMock)
+
+        sut.listGenres { result in
+            guard case .success = result else {
+                XCTFail("Result should be successfull")
+                return
+            }
+        }
+    }
+
     func test_getPopularMovies_shouldBuildExpectedEndpoint() {
         _ = sut.getMovies(1) { _ in }
         let endpoint = apiClientMock.endpoint
@@ -113,6 +130,68 @@ class PMServiceTests: XCTestCase {
                 URLQueryItem(
                     name: "include_adult",
                     value: "false"
+                )
+            ]
+        )
+    }
+
+    func test_listGenres_shouldBuildExpectedEndpoint() {
+        _ = sut.listGenres { _ in }
+        let endpoint = apiClientMock.endpoint
+
+        XCTAssertEqual(endpoint?.host, "api.themoviedb.org")
+        XCTAssertEqual(endpoint?.path, "/3/genre/movie/list")
+        XCTAssertEqual(endpoint?.method, "get")
+        XCTAssertEqual(
+            endpoint?.query,
+            [
+                URLQueryItem(
+                    name: "api_key",
+                    value: "f66bae459e0caf58012f1645bfb5e772"
+                ),
+                URLQueryItem(
+                    name: "language", value: "en-US"
+                )
+            ]
+        )
+    }
+
+    func test_discoverMoviesByGenre_shouldBuildExpectedEndpoint() {
+        _ = sut.listMoviesByGenre(1, 28) { _ in }
+        let endpoint = apiClientMock.endpoint
+
+        XCTAssertEqual(endpoint?.host, "api.themoviedb.org")
+        XCTAssertEqual(endpoint?.path, "/3/discover/movie")
+        XCTAssertEqual(endpoint?.method, "get")
+        XCTAssertEqual(
+            endpoint?.query,
+            [
+                URLQueryItem(
+                    name: "api_key",
+                    value: "f66bae459e0caf58012f1645bfb5e772"
+                ),
+                URLQueryItem(
+                    name: "language", value: "en-US"
+                ),
+                URLQueryItem(
+                    name: "sort_by",
+                    value: "popularity.desc"
+                ),
+                URLQueryItem(
+                    name: "include_adult",
+                    value: "false"
+                ),
+                URLQueryItem(
+                    name: "page",
+                    value: "1"
+                ),
+                URLQueryItem(
+                    name: "with_genres",
+                    value: "28"
+                ),
+                URLQueryItem(
+                    name: "with_watch_monetization_types",
+                    value: "flatrate"
                 )
             ]
         )
