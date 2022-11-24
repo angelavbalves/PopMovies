63fc58e13de8202ae4d71a90160e4f7b60b878ed
Angela Alves <angelavbalves@rede.ulbra.br>
2022-11-24 12:27:22 -0300

    feat:  dark mode button

--- PopMovies/PopMovies/Sources/Views/ListPopMovies/Views/PopMoviesViewController.swift
+++ PopMovies/PopMovies/Sources/Views/ListPopMovies/Views/PopMoviesViewController.swift
@@ -11,6 +11,11 @@ class PopMoviesViewController: PMViewController {
 
     // MARK: -  ViewModel
     private let viewModel: PopMoviesViewModel
+    private var isDarkMode: Bool = false {
+        didSet {
+            setImageButton(isDarkMode)
+        }
+    }
 
     // MARK: - View
     private lazy var rootView = PopMoviesView(
@@ -36,11 +41,47 @@ class PopMoviesViewController: PMViewController {
         super.viewDidLoad()
         title = "Pop Movies"
         getPopMovies()
+        setImageButton(true)
+        navigationItem.searchController = searchBar
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         rootView.reloadCollectionView()
+    }
+
+    // MARK: - Setup dark mode button
+    func setImageButton(_ isSelected: Bool) {
+        let light = UIImage(systemName: "sun.max")
+        let dark = UIImage(systemName: "moon")
+        if isSelected {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(
+                image: dark,
+                style: .done,
+                target: self,
+                action: #selector(chooseTheme)
+            )
+            isDark = true
+        } else {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(
+                image: light,
+                style: .done,
+                target: self,
+                action: #selector(chooseTheme)
+            )
+            isDark = false
+        }
+    }
+
+    @objc func chooseTheme() {
+        configureMode(for: appTheme)
+        if isDark {
+            setImageButton(false)
+            view.window?.overrideUserInterfaceStyle = .dark
+        } else {
+            setImageButton(true)
+            view.window?.overrideUserInterfaceStyle = .light
+        }
     }
 
     // MARK: - Aux
