81f914969b7f3a6f7903e494f30219a2f70cd2bd
Angela Alves <angelavbalves@rede.ulbra.br>
2022-12-23 16:43:07 -0300

    feat: filter movies by name (#21)
    * feat:  dark mode button
    
    * feat: filter movies by name in the api
    
    * feat: uses the filtered movies to populate the view
    
    * chore: xcodeproj

--- PopMovies/PopMovies/Sources/Views/ListPopMovies/Views/PopMoviesViewController.swift
+++ PopMovies/PopMovies/Sources/Views/ListPopMovies/Views/PopMoviesViewController.swift
@@ -11,6 +11,11 @@ class PopMoviesViewController: PMViewController {
 
     // MARK: -  ViewModel
     private let viewModel: PopMoviesViewModel
+    private var isDarkMode: Bool = false {
+        didSet {
+            setImageButton(isDarkMode)
+        }
+    }
 
     // MARK: - View
     private lazy var rootView = PopMoviesView(
@@ -20,6 +25,12 @@ class PopMoviesViewController: PMViewController {
         favoriteButtonUnselectedAction: buttonUnselected(_:),
         verifyIfMovieIsInCoreData: verifyMovie(_:)
     )
+
+    private lazy var searchBar = UISearchController() .. {
+        $0.searchBar.placeholder = "Search Movies By Title"
+        $0.searchResultsUpdater = self
+        $0.searchBar.delegate = self
+    }
 
     // MARK: - Init
     init(viewModel: PopMoviesViewModel) {
@@ -36,11 +47,47 @@ class PopMoviesViewController: PMViewController {
         super.viewDidLoad()
         title = "Pop Movies"
         getPopMovies()
+        setImageButton(true)
+        navigationItem.searchController = searchBar
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         rootView.reloadCollectionView()
+    }
+
+    // MARK: - Setup dark mode button
+    func setImageButton(_ isSelected: Bool) {
+        let light = UIImage(systemName: "sun.max")
+        let dark = UIImage(systemName: "moon")
+        if isSelected {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(
+                image: dark,
+                style: .done,
+                target: self,
+                action: #selector(chooseTheme)
+            )
+            isDark = true
+        } else {
+            navigationItem.leftBarButtonItem = UIBarButtonItem(
+                image: light,
+                style: .done,
+                target: self,
+                action: #selector(chooseTheme)
+            )
+            isDark = false
+        }
+    }
+
+    @objc func chooseTheme() {
+        configureMode(for: appTheme)
+        if isDark {
+            setImageButton(false)
+            view.window?.overrideUserInterfaceStyle = .dark
+        } else {
+            setImageButton(true)
+            view.window?.overrideUserInterfaceStyle = .light
+        }
     }
 
     // MARK: - Aux
@@ -71,5 +118,28 @@ class PopMoviesViewController: PMViewController {
 
     func verifyMovie(_ id: Int) -> Bool {
         viewModel.verifyMovieInCoreData(for: id)
+    }
+}
+
+extension PopMoviesViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text, !text.isEmpty else { return }
+        loadingView.show()
+        viewModel.filterMovies(text) { [weak self] state in
+            switch state {
+                case .success(let movies):
+                    self?.rootView.showSearchResults(movies)
+                    self?.loadingView.hide()
+                case .error:
+                    print("Error to filter movies by title")
+            }
+        }
+    }
+}
+
+extension PopMoviesViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        resignFirstResponder()
+        rootView.resetFilteredMovies()
     }
 }
